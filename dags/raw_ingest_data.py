021b68148e9492aad6e8bac5f815dcee58cce637
jorgemaciel <jorge.maciel.jr@gmail.com>
2024-10-04 07:48:46 -0300

    🗑️ cleanup: download using lib pysus

--- dags/raw_ingest_data.py
+++ dags/raw_ingest_data.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import os
-import urllib.request
 
 import minio
 from airflow import DAG
@@ -20,41 +19,16 @@ dag = DAG(
 )
 
 
-def get_dbc_file(ano, client):
-    url = f'ftp://ftp.datasus.gov.br/dissemin/publicos/SINAN/DADOS/PRELIM/ANIMBR{ano}.dbc'
-    filename = f'ANIMBR{ano}.dbc'
-
-    urllib.request.urlretrieve(url, filename)
-
-    data_dir = os.environ.get('MINIO_DATA_SUS_FOLDER')
-
-    client.fput_object(
-        os.environ.get('MINIO_BUCKET_NAME'),
-        f"{data_dir}/{filename}",
-        filename,
-        content_type='application/octet-stream'
-    )
-
-    # Delete the local file
-    os.remove(filename)
-
-
-def _download_dbc_file():
-    dates = ['2020', '2021', '2022', '2023']
-    client = minio.Minio(
+client = minio.Minio(
         os.environ.get('MINIO_ENDPOINT'),
         access_key=os.environ.get('MINIO_ACCESS_KEY'),
         secret_key=os.environ.get('MINIO_SECRET_KEY'),
         secure=False  # Adjust based on your MinIO configuration
     )
 
-    # Check if bucket exists, create if not
-    if not client.bucket_exists(os.environ['MINIO_BUCKET_NAME']):
-        client.make_bucket(os.environ['MINIO_BUCKET_NAME'])
 
-    for date in dates:
-        date = date[-2:]  # Extract the last two digits of the year
-        get_dbc_file(date, client)
+def _download_sinan_data():
+    pass
 
 
 start = PythonOperator(
@@ -66,7 +40,7 @@ start = PythonOperator(
 
 download_files_task = PythonOperator(
     task_id='Download_dbc_files',
-    python_callable=_download_dbc_file,
+    python_callable=_download_sinan_data,
     dag=dag
 )
 
@@ -76,5 +50,6 @@ end = PythonOperator(
     python_callable=lambda: print("Jobs completed successfully"),
     dag=dag
 )
+
 
 start >> download_files_task >> end
